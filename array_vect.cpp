f1826f5abde6c75210a21c148f7b3636586bf23f
Ryan Bride <rbride@mail.bradley.edu>
2022-09-04 22:41:24 -0400

    Added a comparison function for the student class

--- array_vect.cpp
+++ array_vect.cpp
@@ -5,38 +5,61 @@
 ########################################################################*/
 #include <iostream>
 #include <string.h>
+#include <compare>
 using namespace std;
 
 /* Function Pointer that is used to point towards functions in the container */
 typedef void (*container_fun_ptr)(const void * data_ptr);
-
+typedef int(*container_comparison_ptr)(const void *data_ptr1, const void *data_ptr2);
 //Basic Student Class used to create Students that are added to the Container
-class student
+class Student
 {
   private:
     string name;
     int age;
   public:
     //Constructor
-    student(string s, int i) {
+    Student(string s, int i) {
       name = s; 
       age = i;
     }
-    string getName() { return name; } 
-    int getAge() { return age; }
+    string getName() const { return name; } 
+    int getAge() const { return age; }
 };
+/* Function used to sort Student Items in the container */
+int student_sort_cmp( const void * x, const void * y) {
+  //Cast the void pointers to student
+  Student stu_x = *((Student *) x);
+  Student stu_y = *((Student *) y);
+  /* First Check by age then by name, if the same 0, 1 means it goes after
+  -1 means it comes before, these are the required inputs for the binsearch */
+  if (stu_x.getAge() < stu_y.getAge())
+    return( -1 );
+  else if (stu_x.getAge() > stu_y.getAge()) 
+    return( 1 );
+  else {
+    int temp = stu_x.getName().compare(stu_y.getName());
+    if(temp == 0)
+      return( 0 );
+    else if (temp < 0)
+      return( -1 );
+    else 
+      return( 1 );
+    }
+}
 
 /* Print student info, prints the info of the student after being given the 
 ptr to said info from the class and told to use a double pointer by the struct/class
 first cast a variable, temp_stu of type student, that is set equal to the dereferenced
 data_ptr that was cast to a student pointer. then simply prints name and age */
 void print_student_info(const void * data_ptr){
-  student temp_stu = *((student *) data_ptr);
+  Student temp_stu = *((Student *) data_ptr);
   cout << temp_stu.getName() << ' ' << temp_stu.getAge() << '\t' ;
 }
 
 struct container
 {
+  container_comparison_ptr comparator; //used to define the comparison function used to sort container
   void **arr;        // pointer to array of pointer to void
   void *item_ptr;
   int allocation;    // current size of the array
@@ -44,7 +67,8 @@ struct container
   //Type used  for map function, / Student pointer
   
   // Construct the array of size
-  container( ) {
+  container(container_comparison_ptr comp) {
+    comparator = comp;
     allocation = 3; 
     size = 0;       
     arr = (void **)malloc(allocation);
@@ -73,7 +97,7 @@ struct container
     }
 
   }
-  /* Maps the print_student_info to print student info (NOTE: STUDENT CLASS SPECIFIC) */
+  /* Maps the print_Student_info to print student info (NOTE: STUDENT CLASS SPECIFIC) */
   void print() {
     map( (void (*) (const void *)) print_student_info);
     cout << '\n'; //After Printing new line for sake of output readablity
@@ -88,11 +112,13 @@ struct container
 
 };
 
+
+
 int main()
 {
-  struct container test_vect;
-  student joe("Joe Smith", 33), chris("Chris Jones", 52);
-  student andy("Andy Stelanski", 21), tony("Tony Vilory", 67);
+  struct container test_vect( (int (*) (const void *, const void *))student_sort_cmp);
+  Student joe("Joe Smith", 33), chris("Chris Jones", 52);
+  Student andy("Andy Stelanski", 21), tony("Tony Vilory", 67);
   void *stu_ptr = &joe;
   test_vect.insert(stu_ptr);
   test_vect.print();
@@ -105,8 +131,6 @@ int main()
   test_vect.insert(stu_ptr);
   test_vect.print();
 
-
-  return 0;
 }
 // int z = 12;
 // int *i = &z;
